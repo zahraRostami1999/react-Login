ee923558a0d7f9d6be3c37a360847d04558a1709
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-25 17:05:25 +0330

    install font awsome

--- react-Login/src/pages/RegisterPage.jsx
+++ react-Login/src/pages/RegisterPage.jsx
@@ -3,7 +3,7 @@ import {Link}  from "react-router-dom";
 const RegisterPage = () => {
     return (
         <>
-            <div className='bg-pink-300 h-screen flex justify-center items-center'>
+            <div className='bg-pink-300 h-screen flex justify-center items-center font-sans'>
                 <div className='w-1/3 bg bg-black-opacity-5 h-2/3 rounded'>
                     <h1 className="font-semibold text-2xl mx-10 my-10">Register</h1>
                     <div className="grid grid-rows-5 grid-cols-1 gap-5 mx-10 rounded mt-10">
