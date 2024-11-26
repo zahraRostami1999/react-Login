4b6c4119f28deeb6e58d3def2eb1c677a78bfd5e
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-26 11:59:33 +0330

    edit App: set Routes

--- react-Login/src/App.jsx
+++ react-Login/src/App.jsx
@@ -1,10 +1,19 @@
-import RegisterPage from "./pages/RegisterPage"
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import RegisterPage from "./pages/RegisterPage";
+import CatFactsPage from "./pages/CatFactsPage";
 
 function App() {
 
   return (
     <>
-    <RegisterPage />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<RegisterPage />} />
+          <Route path="/cat-facts" element={<CatFactsPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
