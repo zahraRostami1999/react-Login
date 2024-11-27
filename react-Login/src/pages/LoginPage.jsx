edbe61aaf693f9c54f0e1703218dd0d9d22cd4e2
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-27 19:33:06 +0330

    edit LoginPage: design

--- react-Login/src/pages/LoginPage.jsx
+++ react-Login/src/pages/LoginPage.jsx
@@ -2,7 +2,16 @@ const LoginPage = () => {
     return (
         <>
         <div>
-            <h1>This Is Login Page</h1>
+            <div className='bg-pink-300 h-screen flex justify-center items-center font-sans'>
+                <div className='md:w-2/3 lg:w-1/3 bg bg-black-opacity-5 lg:h-2/3 rounded sm:w-5/6 sm:h-5/6 md:h-5/6'>
+                    <h1 className="font-semibold text-2xl mx-10 my-8">Login</h1>
+                    <div className="rounded mt-10">
+                        <input className="rounded-xl px-2 h-12  border-none text-base required block w-5/6 my-10 mx-auto" type='text' placeholder='Username'  />
+                        <input className="rounded-xl px-2 h-12 border-none text-base required block w-5/6 my-10 mx-auto" type='password' placeholder='Password' />
+                        <button className="bg-orange-600 rounded-xl block w-5/6 h-12 my-10 mx-auto">Sign Up</button>
+                    </div>
+                </div>
+            </div>
         </div>
         </>
     )
