452bf6b928720e40f396d91bc45e359baec1e912
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-27 22:21:31 +0330

    edit App: import HomePage

--- react-Login/src/App.jsx
+++ react-Login/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import HomePage from './pages/HomePage';
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import CatFactsPage from "./pages/CatFactsPage";
@@ -12,7 +13,8 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path="/" element={<RegisterPage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/cat-facts" element={<CatFactsPage />} />
           <Route path='/test-cookie' element={<TestCookiePage />}></Route>
