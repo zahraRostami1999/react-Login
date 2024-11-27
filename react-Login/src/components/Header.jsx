4bf9f25c443b9d086a8320746d46305b04fb24be
Zahra Rostami <zahrarostami137878@gmail.com>
2024-11-27 22:22:49 +0330

    edit Header: add Home and edit register path

--- react-Login/src/components/Header.jsx
+++ react-Login/src/components/Header.jsx
@@ -4,7 +4,8 @@ const Header = ()=>{
         <>
         <div className="flex justify-center">
             <ul className="flex justify-between py-5 sm:w-11/12 md:w-1/2 lg:w-1/4">
-                <Link to="/">Register</Link>
+                <Link to="/">Home</Link>
+                <Link to="/register">Register</Link>
                 <Link to="/cat-facts">Cat Fact</Link>
                 <Link to="/test-cookie">Test Cookie</Link>
             </ul>
