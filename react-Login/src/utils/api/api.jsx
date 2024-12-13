f4cba212e6936fce9f62a1e2ee6481f2e55cf367
Zahra Rostami <zahrarostami137878@gmail.com>
2024-12-13 21:42:45 +0330

    edit api: connect to fake store api

--- react-Login/src/utils/api/api.jsx
+++ react-Login/src/utils/api/api.jsx
@@ -1,21 +1,69 @@
 import axios from "axios";
-
 export const addUser = (data) => {
-    axios.post('http://localhost:3500/User', {
-        username: data.username,
-        password: data.password,
+    fetch('https://fakestoreapi.com/users', {
+        method: "POST",
+        body: JSON.stringify(
+            {
+                email: 'Johna@gmail.com',
+                username: 'johndaa',
+                password: 'm36rmF$',
+                name: {
+                    firstname: 'Johni',
+                    lastname: 'Doee'
+                },
+                address: {
+                    city: 'kilcoole',
+                    street: '7835 new road',
+                    number: 3,
+                    zipcode: '12926-3864',
+                    geolocation: {
+                        lat: '-37.3119',
+                        long: '81.1396'
+                    }
+                },
+                phone: '1-570-235-7033'
+            }
+        )
     })
-    alert("User added!")
 }
 
-export const verifyUser = async (data) => {
+
+// export const addUser = (data) => {
+//     axios.post('https://fakestoreapi.com/auth/login', {
+//         address: {
+//             city: "",
+//             geolocation:{
+//                 lat:"",
+//                 long:""
+//             },
+//             number: "",
+//             street:"",
+//             zipcode: ""
+//         },
+//         email: "",
+//         id: "11",
+//         name: {
+//             firstname: "",
+//             lastname: ""
+//         },
+//         password: data.password,
+//         phone:"",
+//         username: data.username,
+//         __v: 0
+//     })
+//     alert("User added!")
+// }
+
+export const verifyUser = async (info) => {
     try {
-        const response = await axios.get('http://localhost:3500/User');
-        const users = response.data;
-        const ckeckUser = users.find(user => user.username === data.username && user.password === data.password);
+        const { data } = await axios.get('https://fakestoreapi.com/users');
+        console.log(data);
+        const ckeckUser = data.find(user => user.username === info.username && user.password === info.password);
         return ckeckUser;
-    } catch (error) {
+    } catch {
         console.error('Error verifying user:', error);
         return false;
     }
 };
+
+
